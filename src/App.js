16b9f499e6609b9400b79b975ffdffb7583158c7
Ana Souza <anamariacostasouza93@gmail.com>
2025-04-01 22:20:21 -0300

    Passando eventos por props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form.js'
+import Evento from './components/Evento.js';
 
 function App() {
   return (
     <div className="App">
         <Form></Form>
+        <Evento></Evento>
     </div>
   );
 }
